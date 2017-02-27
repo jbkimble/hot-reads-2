5a324b2b1857cc585a11978487bd98a27fe11f38
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 15:07:25 -0700

    Render top links from last 24 as instance var

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
 
   def index
-
+    @count = 0
+    urls = Url.top_read_in_last_day
+    @urls = urls.sort_by {|key, value| value}.reverse
   end
 
 end
