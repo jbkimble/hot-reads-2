295f1225b23e9d9af9465fc2be9b0b33eada55c5
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 15:08:10 -0700

    Basic functionality working locally

---
+++ app/controllers/api/v1/urls_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::UrlsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    url = Url.new(url: params["url"])
+    if url.save
+      render json: url, :only => [:id, :url]
+    else
+    end
+  end
+end
