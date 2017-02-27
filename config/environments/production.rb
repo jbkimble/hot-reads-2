6f18de547bc2618ee71b8a5fde8b78f178cd12f3
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 16:07:32 -0700

    Test cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #added for cors purposes
+  config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+  config.serve_static_assets = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
