1176ab03464deba8448254ccadb2d940fb91fb52
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 16:00:32 -0700

    Add error log

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
+    puts params.inspect
     url = Url.new(url: params["url"])
     if url.save
       render json: url, :only => [:id, :url]
