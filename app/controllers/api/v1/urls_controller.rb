387b716edcae4a9499084766926b47451688318c
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 18:21:58 -0700

    this

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -3,10 +3,16 @@ class Api::V1::UrlsController < ApplicationController
 
   def create
     puts params.inspect
-    url = Url.new(url: params["url"])
+    url = Url.new(url: url_params)
     if url.save
       render json: url, :only => [:id, :url]
     else
     end
+  end
+
+  private
+
+  def url_params
+    params.permit(:url)
   end
 end
