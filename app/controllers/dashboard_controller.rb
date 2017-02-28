55f637219e6d759c8e2b477df86e4143d4601a5a
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 19:27:42 -0700

    Add production link

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,11 @@
 class DashboardController < ApplicationController
 
   def index
-    uri = URI("http://localhost:3000/api/v1/hots/")
+    uri = URI("https://arcane-fortress-52437.herokuapp.com/api/v1/hots/")
     response = Net::HTTP.get(uri)
     urls = JSON.parse(response)
     @urls = urls.sort_by {|key, value| value}.reverse
     @count = 0
-    # urls = Url.top_read_in_last_day
-    # @urls = urls.sort_by {|key, value| value}.reverse
   end
 
 end
