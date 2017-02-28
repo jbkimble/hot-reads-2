014b7cd87ea1357fcf3aa802aec414d7003ca294
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 17:07:16 -0700

    this

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    puts "boooyah"
+    puts params.inspect
     url = Url.new(url: params["url"])
     if url.save
       render json: url, :only => [:id, :url]
