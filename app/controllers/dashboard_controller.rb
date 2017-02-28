2eef3464e330899be8ba74ab79ca8787fc258061
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 19:20:08 -0700

    Finish api call to get data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,13 @@
 class DashboardController < ApplicationController
 
   def index
-    @count = 0
-    urls = Url.top_read_in_last_day
+    uri = URI("http://localhost:3000/api/v1/hots/")
+    response = Net::HTTP.get(uri)
+    urls = JSON.parse(response)
     @urls = urls.sort_by {|key, value| value}.reverse
+    @count = 0
+    # urls = Url.top_read_in_last_day
+    # @urls = urls.sort_by {|key, value| value}.reverse
   end
 
 end
