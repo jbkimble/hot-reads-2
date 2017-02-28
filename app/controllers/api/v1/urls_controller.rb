a26fe36e54a02c153d3f9cca4e88f7a202e4c216
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-27 18:52:28 -0700

    Abandon api request

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -2,19 +2,11 @@ class Api::V1::UrlsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    puts params.inspect
-    x = url_params["url"]
-    puts "THIS IS IS IMPORTANT:" + x
-    url = Url.new(url: x)
+    url = Url.new(url: params["url"])
     if url.save
       render json: url, :only => [:id, :url]
     else
     end
   end
 
-  private
-
-  def url_params
-    params.permit(:url)
-  end
 end
